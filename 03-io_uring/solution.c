c11b5950d167e7bdcee30a63364d8708aecc7fd5
MamaCheli <lopatinartem1@gmail.com>
2023-10-22 16:12:13 +0300

    delete readv/writev

--- 03-io_uring/solution.c
+++ 03-io_uring/solution.c
@@ -15,22 +15,99 @@
 #define QUEUE_SIZE 4
 #define BLOCK_MAX_SIZE (256 * 1024)
 
+// struct io_data {
+//     int is_read;
+//     off_t first_offset, second_offset;
+//     size_t size;
+//     // struct iovec iov;
+//     char* buf;
+// };
+
+// void queue_prepare(struct io_uring* ring, struct io_data* data, int infd, int outfd) {
+//     struct io_uring_sqe* sqe;
+//     sqe = io_uring_get_sqe(ring);
+//     assert(sqe);
+
+//     if (data->is_read) {
+//         // io_uring_prep_readv(sqe, infd, &data->iov, 1, data->second_offset);
+//         io_uring_prep_read(sqe, infd, data->buf + data->second_offset,
+//                            data->size - data->second_offset,
+//                            data->first_offset + data->second_offset);
+//     } else {
+//         // io_uring_prep_writev(sqe, outfd, &data->iov, 1, data->second_offset);
+//         io_uring_prep_write(sqe, outfd, data->buf + data->second_offset,
+//                             data->size - data->second_offset,
+//                             data->first_offset + data->second_offset);
+//     }
+
+//     io_uring_sqe_set_data(sqe, data);
+//     return;
+// }
+
+// int queue_read(struct io_uring* ring, off_t size, off_t offset, int infd) {
+//     struct io_uring_sqe *sqe;
+//     struct io_data *data;
+
+//     if (!(data = malloc(size + sizeof(struct io_data)))) {
+//         return 1;
+//     }
+//     if (!(sqe = io_uring_get_sqe(ring))) {
+//         free(data);
+//         return 1;
+//     }
+
+//     data->is_read = 1;
+//     data->first_offset = offset;
+//     data->second_offset = offset;
+//     data->size = size;
+//     // data->iov.iov_base = data + 1;
+//     // data->iov.iov_len = size;
+//     data->buf = (char*) data + sizeof(struct io_data);
+
+//     // io_uring_prep_readv(sqe, infd, &data->iov, 1, offset);
+//     io_uring_prep_read(sqe, infd, data->buf, size, offset);
+
+//     io_uring_sqe_set_data(sqe, data);
+//     return 0;
+// }
+
+// void queue_write(struct io_uring* ring, struct io_data* data, int infd, int outfd) {
+//     data->is_read = 0;
+//     data->second_offset = data->first_offset;
+//     // data->iov.iov_base = data + 1;
+//     // data->iov.iov_len = data->size;
+
+//     queue_prepare(ring, data, infd, outfd);
+//     // io_uring_prep_write(p_sqe, outfd, data->buf, data->size, data->first_offset);
+//     io_uring_submit(ring);
+//     return;
+// }
+
 struct io_data {
     int is_read;
     off_t first_offset, second_offset;
     size_t size;
-    struct iovec iov;
+    // struct iovec iov;
+    char* buf;
 };
 
-void queue_prepare(struct io_uring* ring, struct io_data* data, int infd, int outfd) {
+void queue_prepare(struct io_uring* ring, struct io_data* data, int infd, int outfd, off_t process_len) {
     struct io_uring_sqe* sqe;
     sqe = io_uring_get_sqe(ring);
     assert(sqe);
 
+    data->second_offset += process_len;
+
     if (data->is_read) {
-        io_uring_prep_readv(sqe, infd, &data->iov, 1, data->second_offset);
+        // io_uring_prep_readv(sqe, infd, &data->iov, 1, data->second_offset);
+        io_uring_prep_read(sqe, infd, data->buf + data->second_offset,
+                           data->size - data->second_offset,
+                           data->first_offset + data->second_offset);
     } else {
-        io_uring_prep_writev(sqe, outfd, &data->iov, 1, data->second_offset);
+        // io_uring_prep_writev(sqe, outfd, &data->iov, 1, data->second_offset);
+        io_uring_prep_write(sqe, outfd, data->buf + data->second_offset,
+                            data->size - data->second_offset,
+                            data->first_offset + data->second_offset);
     }
 
     io_uring_sqe_set_data(sqe, data);
@@ -51,25 +128,30 @@ int queue_read(struct io_uring* ring, off_t size, off_t offset, int infd) {
 
     data->is_read = 1;
     data->first_offset = offset;
-    data->second_offset = offset;
+    data->second_offset = 0;
     data->size = size;
-    data->iov.iov_base = data + 1;
-    data->iov.iov_len = size;
+    // data->iov.iov_base = data + 1;
+    // data->iov.iov_len = size;
+    data->buf = (char*) data + sizeof(struct io_data);
 
-    io_uring_prep_readv(sqe, infd, &data->iov, 1, offset);
+    // io_uring_prep_readv(sqe, infd, &data->iov, 1, offset);
+    io_uring_prep_read(sqe, infd, data->buf, size, offset);
     io_uring_sqe_set_data(sqe, data);
     return 0;
 }
 
-void queue_write(struct io_uring* ring, struct io_data* data, int infd, int outfd) {
+void queue_write(struct io_uring* ring, struct io_data* data, int outfd) {
     data->is_read = 0;
-    data->second_offset = data->first_offset;
-    data->iov.iov_base = data + 1;
-    data->iov.iov_len = data->size;
+    data->second_offset = 0;
+    // data->iov.iov_base = data + 1;
+    // data->iov.iov_len = data->size;
 
-    queue_prepare(ring, data, infd, outfd);
+    struct io_uring_sqe* sqe = io_uring_get_sqe(ring);
+    assert(sqe);
+
+    io_uring_prep_write(sqe, outfd, data->buf, data->size, data->first_offset);
+    io_uring_sqe_set_data(sqe, data);
     io_uring_submit(ring);
-    return;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -151,19 +233,19 @@ int copy(int in, int out) {
             data = io_uring_cqe_get_data(cqe);
             if (cqe->res < 0) {
                 if (cqe->res == -EAGAIN) {
-                    queue_prepare(&ring, data, in, out);
+                    queue_prepare(&ring, data, in, out, 0);
                     io_uring_cqe_seen(&ring, cqe);
                     continue;
                 }
                 return cqe->res;
             } else if ((size_t) cqe->res != data->size) {
-                queue_prepare(&ring, data, in, out); 
+                queue_prepare(&ring, data, in, out, cqe->res); 
                 io_uring_cqe_seen(&ring, cqe);
                 continue;
             }
 
             if (data->is_read) {
-                queue_write(&ring, data, in, out);
+                queue_write(&ring, data, out);
                 write_size -= data->size;
                 reads--;
                 writes++;
